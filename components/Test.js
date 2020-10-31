eba5d7df96feb07d31877c58859ee59604a41037
AlecxMoritz <alecxmoritz7@gmail.com>
2020-10-30 23:08:25 -0400

    remove test component

--- components/Test.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const TestComponent = () => {
-  const placeholder = 'Hello alecx';
-  const test = 'yoyo';
-  return <div>{placeholder + test}</div>;
-};
-
-export default TestComponent;
