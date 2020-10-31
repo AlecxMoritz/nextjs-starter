2e7f86f7406217963fca7b281759c8668b58de6b
AlecxMoritz <alecxmoritz7@gmail.com>
2020-10-30 23:08:00 -0400

    add prettier

---
+++ components/Test.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const TestComponent = () => {
+  const placeholder = 'Hello alecx';
+  const test = 'yoyo';
+  return <div>{placeholder + test}</div>;
+};
+
+export default TestComponent;
